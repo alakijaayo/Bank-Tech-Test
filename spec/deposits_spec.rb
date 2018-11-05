1213ed70d9e497cd99a707ed4209912ec0ea02b0
alakijayo <alakijaayo@gmail.com>
2018-11-05 17:48:53 +0000

    Testing Deposits and Withdraw classes in own spec

---
+++ spec/deposits_spec.rb
@@ -0,0 +1,19 @@
+require './lib/bank'
+require './lib/deposits'
+
+describe Deposits do
+
+
+  it "stores data from our bank class" do
+    bank = Bank.new
+    bank.deposit(10)
+    expect(bank.money_in.history).to eq [10]
+  end
+
+  it "allows the person view the deposits they haver made" do
+    bank = Bank.new
+    bank.deposit(10)
+    bank.deposit(20)
+    expect(bank.deposits).to eq [10, 20]
+  end
+end
---
+++ spec/withdraw_spec.rb
@@ -0,0 +1,21 @@
+require './lib/bank'
+require './lib/withdraw'
+
+describe Deposits do
+
+
+  it "stores data from our bank class" do
+    bank = Bank.new
+    bank.deposit(30)
+    bank.withdraw(10)
+    expect(bank.money_out.history).to eq [-10]
+  end
+
+  it "allows the person view the deposits they haver made" do
+    bank = Bank.new
+    bank.deposit(50)
+    bank.withdraw(10)
+    bank.withdraw(20)
+    expect(bank.withdraws).to eq [-10, -20]
+  end
+end
